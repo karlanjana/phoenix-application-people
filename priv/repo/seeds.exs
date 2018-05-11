22fde64cc7fb143e02a2ba2115fa34f2e32cb3dd
karlanjana <karlanjana@gmail.com>
2018-05-11 05:30:50 +0100

    Add Changes To Seeds File To Parse Csv File And Save People Into The Database

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,20 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+defmodule People.Seeds do
+
+	alias People.Person
+	alias People.Repo
+	
+	def add_person({:ok ,data_row}) do 
+		changeset = Person.changeset(%Person{}, data_row)
+		Repo.insert!(changeset)
+	end
+
+end
+
+File.stream!("assets/static/people.csv")
+	|> Stream.drop(1)
+	|> CSV.decode(headers: [:name, :title, :location, :photo])
+	|> Enum.each(&People.Seeds.add_person/1)
